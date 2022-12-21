4ce4085b7ca32e21a6cc68d1dd46075a990bf84a
hoang1306 <donphuong.chanphuong@gmail.com>
2022-12-21 22:17:28 +0700

    rotation and translation an images

--- learnOpenCv_basics_1/README.md
+++ learnOpenCv_basics_1/README.md
@@ -3,3 +3,5 @@
 <h2>Lesson 1. read, display and write an image using opencv (file code: <a href="#">RDW.cpp</a>)</h2>
 <h2>Lesson 2. reading, writing video using opencv (file code: <a href="#">R_and_W_video_part_%2.cpp</a>)</h2>
 <h2>Lesson 3. Image resizing (file code: <a href="#">img_resizing.cpp</a>)</h2>
+<h2>Lesson 4. cropping an images (file code: <a href="#">cropping_an_images.cpp</a>)</>
+<h2>Lesson 5. Image rotation and transforming (file code : <a href="#">img_rotation_and_translation.cpp</a>)</h2>
---
+++ learnOpenCv_basics_1/img_rotation_and_translation.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+// 1. rotation images
+// 2. translation images
+
+int main(){
+    std::string image_path = "../images/rotation.png";
+    cv::Mat img = cv::imread(image_path);
+    // 1. rotation images
+    cv::Point2f center((img.cols - 1)/2., (img.rows - 1)/2.);
+    // std::cout << center << std::endl;
+    cv::Mat rotation_matrix = cv::getRotationMatrix2D(center, 45, 1.0);
+    // std::cout << rotation_matrix << std::endl;
+    cv::Mat rotated_image;
+    cv::warpAffine(img, rotated_image,rotation_matrix, img.size());
+    // imshow("rotation images", rotated_image);
+    // 2. translation images
+    int height = img.cols;
+    int width = img.rows;
+
+    float tx = -float(width)/3;
+    float ty = -float(height)/5;
+    
+    float warp_values[] = {1.0, 0.0, tx, 0.0, 1.0, ty};
+    cv::Mat translation_img;
+    cv::Mat translation_matrix = cv::Mat(2, 3, CV_32F, warp_values);
+    cv::warpAffine(img, translation_img,translation_matrix, img.size());
+    imshow("translation image", translation_img);
+    cv::waitKey(0);
+    return 0;
+}
